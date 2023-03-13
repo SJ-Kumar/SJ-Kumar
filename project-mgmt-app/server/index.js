e549fdf4f3e71393e2463f25172ed718357bf2fb
Suresh <=>
2023-03-13 20:20:37 +0530

    update

--- project-mgmt-app/server/index.js
+++ project-mgmt-app/server/index.js
@@ -5,7 +5,7 @@ require('dotenv').config();
 const { graphqlHTTP } = require('express-graphql');
 const schema = require('./schema/schema');
 const connectDB = require('./config/db');
-const port = process.env.PORT || 8000;
+const port = process.env.PORT || 4000;
 
 const app = express();
 
