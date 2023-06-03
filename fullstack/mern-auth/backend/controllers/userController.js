f30075d7816221e447419e25382226185f6f44ba
Jayanth Kumar <jayanthkumar597@gmail.com>
2023-06-03 22:14:00 +0530

    updated

--- fullstack/mern-auth/backend/controllers/userController.js
+++ fullstack/mern-auth/backend/controllers/userController.js
@@ -5,8 +5,40 @@ import asyncHandler from 'express-async-handler'
 // @access  Public
 const authUser =  asyncHandler(async (req, res) => {
      res.status(200).json({ message: 'Auth User' })
+});
+
+// @desc    Register a new user
+// @route   POST /api/users
+// @access  Public
+const registerUser = asyncHandler(async (req, res) => {
+    res.status(200).json({ message: 'Register User' })
+});
+
+// @desc    Logout user / clear cookie
+// @route   POST /api/users/logout
+// @access  Public
+const logoutUser = asyncHandler(async (req, res) => {
+    res.status(200).json({ message: 'Logout User' })
 
 });
 
-export { authUser };
+// @desc    Get user profile
+// @route   GET /api/users/profile
+// @access  Private
+const getUserProfile = asyncHandler(async (req, res) => {
+    res.status(200).json({ message: 'User Profile' })
+});
 
+// @desc    Update user profile
+// @route   PUT /api/users/profile
+// @access  Private
+const updateUserProfile = asyncHandler(async (req, res) => {
+    res.status(200).json({ message: 'Update user Profile' })
+});
+    export {
+        authUser,
+        registerUser,
+        logoutUser,
+        getUserProfile,
+        updateUserProfile,
+      };
