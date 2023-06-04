225aa33a346691f30955ddef4eb824a71311e48a
Jayanth Kumar <jayanthkumar597@gmail.com>
2023-06-04 15:41:05 +0530

    updated

--- fullstack/mern-auth/backend/controllers/userController.js
+++ fullstack/mern-auth/backend/controllers/userController.js
@@ -1,4 +1,5 @@
 import asyncHandler from 'express-async-handler'
+import generateToken from '../utils/generateToken.js';
 import User from '../models/userModel.js';
 
 // @desc    Auth user & get token
@@ -30,6 +31,7 @@ const registerUser = asyncHandler(async (req, res) => {
   });
 
   if (user) {
+      generateToken(res, user._id)
       res.status(201).json({
         _id: user._id,
         name: user.name,
