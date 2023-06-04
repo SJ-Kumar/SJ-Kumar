7723c329a6108dc6f4dae2c81e5e3d5c94e2fa71
Jayanth Kumar <jayanthkumar597@gmail.com>
2023-06-04 15:12:04 +0530

    updated

--- fullstack/mern-auth/backend/controllers/userController.js
+++ fullstack/mern-auth/backend/controllers/userController.js
@@ -12,9 +12,35 @@ const authUser =  asyncHandler(async (req, res) => {
 // @route   POST /api/users
 // @access  Public
 const registerUser = asyncHandler(async (req, res) => {
-    console.log(req.body);
+   
+    const { name, email, password } = req.body;
+    console.log(name, email, password);
+
+    const userExists = await User.findOne({ email });
+
+  if (userExists) {
+    res.status(400);
+    throw new Error('User already exists');
+  }
+
+  const user = await User.create({
+    name,
+    email,
+    password,
+  });
+
+  if (user) {
+      res.status(201).json({
+        _id: user._id,
+        name: user.name,
+        email: user.email 
+
+      })
+    } else {
+        res.status(400)
+        throw new Error ('Invalid user data')
+    }
     
-    res.status(200).json({ message: 'Register User' });
 });
 
 // @desc    Logout user / clear cookie
