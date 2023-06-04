3c892675ca2d060d64c97a8b1eaa597025b4bb2c
Jayanth Kumar <jayanthkumar597@gmail.com>
2023-06-04 12:49:32 +0530

    Updated

--- fullstack/mern-auth/backend/controllers/userController.js
+++ fullstack/mern-auth/backend/controllers/userController.js
@@ -1,24 +1,27 @@
 import asyncHandler from 'express-async-handler'
+import User from '../models/userModel.js';
 
 // @desc    Auth user & get token
 // @route   POST /api/users/auth
 // @access  Public
 const authUser =  asyncHandler(async (req, res) => {
-     res.status(200).json({ message: 'Auth User' })
+     res.status(200).json({ message: 'Auth User' });
 });
 
 // @desc    Register a new user
 // @route   POST /api/users
 // @access  Public
 const registerUser = asyncHandler(async (req, res) => {
-    res.status(200).json({ message: 'Register User' })
+    console.log(req.body);
+    
+    res.status(200).json({ message: 'Register User' });
 });
 
 // @desc    Logout user / clear cookie
 // @route   POST /api/users/logout
 // @access  Public
 const logoutUser = asyncHandler(async (req, res) => {
-    res.status(200).json({ message: 'Logout User' })
+    res.status(200).json({ message: 'Logout User' });
 
 });
 
@@ -26,14 +29,14 @@ const logoutUser = asyncHandler(async (req, res) => {
 // @route   GET /api/users/profile
 // @access  Private
 const getUserProfile = asyncHandler(async (req, res) => {
-    res.status(200).json({ message: 'User Profile' })
+    res.status(200).json({ message: 'User Profile' });
 });
 
 // @desc    Update user profile
 // @route   PUT /api/users/profile
 // @access  Private
 const updateUserProfile = asyncHandler(async (req, res) => {
-    res.status(200).json({ message: 'Update user Profile' })
+    res.status(200).json({ message: 'Update user Profile' });
 });
     export {
         authUser,
